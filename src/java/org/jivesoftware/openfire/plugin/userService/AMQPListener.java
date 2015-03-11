0ef0763431c6c37e01c297396fa7a92f0eb5031b
Dusan Klinec <dusan.klinec@gmail.com>
2015-03-11 10:36:49 +0100

    AMQP listener refactored - better logic, logs.

--- src/java/org/jivesoftware/openfire/plugin/userService/AMQPListener.java
+++ src/java/org/jivesoftware/openfire/plugin/userService/AMQPListener.java
@@ -14,139 +14,150 @@ import org.slf4j.LoggerFactory;
 
 /**
  * AMPQ listener.
+ *
  * @author dusanklinec
  */
 public class AMQPListener extends BackgroundThreadService {
-   private static final Logger log = LoggerFactory.getLogger(AMQPListener.class);
-   private static final String QUEUE_SERVER     = "phone-x.net";
-   private static final String QUEUE_VHOST      = "/phonex"; 
-   private static final String QUEUE_USER_NAME  = "xmppServer";
-   private static final String QUEUE_XMPP_NAME  = "xmpp"; 
-   
-   /**
-    * Timeous sync in milliseconds.
-    */
-   private static final long TIMEOUT_LISTEN = 1000*5;
-   private long lastSync = 0;
-   private volatile boolean running=true;
-   private volatile boolean connectionOK=false;
-   
-   private ConnectionFactory factory;
-   private Connection connection;
-   private Channel channel;
-   private AMQPMsgListener listener;
-    
-   /**
-    * Initializes internal running thread.
-    */
-   public synchronized void init() {
-       initThread(this, "AMQPListener");
-       
-       factory = new ConnectionFactory();
-       factory.setHost(QUEUE_SERVER);
-       factory.setUsername(QUEUE_USER_NAME);
-       factory.setPassword("chooshew5ot1aeMi");
-       factory.setVirtualHost(QUEUE_VHOST);
-       
-       log.info("AMPQ connected & declared");
-       this.start();
-   }
-   
-   public synchronized void deinit(){
-       log.info("listener deinitialization");
-       setRunning(false);
-   }
-   
-   private void ensureConnected(){
-       boolean connected = false;
-       try {
-           connected = connection != null && connection.isOpen();
-       } catch(Exception e){
-           log.error("Cannot determine if connected or not", e);
-       }
-       
-       if (connected){
-           return;
-       }
-       
-       try {
+    private static final Logger log = LoggerFactory.getLogger(AMQPListener.class);
+    private static final String QUEUE_SERVER = "phone-x.net";
+    private static final String QUEUE_VHOST = "/phonex";
+    private static final String QUEUE_USER_NAME = "xmppServer";
+    private static final String QUEUE_XMPP_NAME = "xmpp";
+
+    /**
+     * Timeous sync in milliseconds.
+     */
+    private static final long TIMEOUT_LISTEN = 1000 * 5;
+    private long lastSync = 0;
+    private volatile boolean running = true;
+    private volatile boolean connectionOK = false;
+
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
+    private AMQPMsgListener listener;
+
+    /**
+     * Initializes internal running thread.
+     */
+    public synchronized void init() {
+        initThread(this, "AMQPListener");
+
+        factory = new ConnectionFactory();
+        factory.setHost(QUEUE_SERVER);
+        factory.setUsername(QUEUE_USER_NAME);
+        factory.setPassword("chooshew5ot1aeMi");
+        factory.setVirtualHost(QUEUE_VHOST);
+
+        log.info(String.format("AMPQ starting: %s", this));
+        this.start();
+    }
+
+    public synchronized void deinit() {
+        log.info(String.format("Deinitializing AMQP listener, this=%s", this));
+        setRunning(false);
+    }
+
+    private void ensureConnected() {
+        boolean connected = false;
+        try {
+            connected = connection != null && connection.isOpen() && channel != null && channel.isOpen();
+        } catch (Exception e) {
+            log.error("Cannot determine if connected or not", e);
+        }
+
+        if (connected) {
+            return;
+        }
+
+        try {
             connection = factory.newConnection();
             channel = connection.createChannel();
             channel.queueDeclare(QUEUE_XMPP_NAME, true, false, false, null);
             connectionOK = true;
-       } catch(Exception e){
-           log.error("Cannot initialize messaging queue...");
-           connectionOK = false;
-       }    
-   }
+            log.info(String.format("AMQP connected, listener=%s, connection=%s, channel=%s", this, connection, channel));
 
-   public void doTheJob(){
-       try {
-           this.ensureConnected();
-           
-           final QueueingConsumer consumer = new QueueingConsumer(channel);
-           channel.basicConsume(QUEUE_XMPP_NAME, true, consumer);
-           while (this.running) {
-              final QueueingConsumer.Delivery delivery = consumer.nextDelivery(2000);
-              if (delivery == null){
-                  continue;
-              }
-              
-              processNewMessage(delivery);
-           }
-           
-           // Close channel.
-           if (channel != null){
-               try {
-                   channel.close();
-               } catch(Exception ex){
-                   log.error("Could not close the channel", ex);
-               }
-           }
-           
-           // Close connection.
-           if (connection != null){
-               try {
-                   connection.close();
-               } catch(Exception ex){
-                   log.error("Could not close the connection", ex);
-               }
-           }
-        } catch(Exception ex){
-            log.warn("Problem occurred during property sync", ex);
+        } catch (Exception e) {
+            log.error("Cannot initialize messaging queue...");
+            connectionOK = false;
         }
-   }
-   
+    }
+
+    private void ensureClosed() {
+        // Close channel.
+        if (channel != null) {
+            try {
+                log.info(String.format("Going to close channel: %s", channel));
+
+                channel.close();
+                channel = null;
+            } catch (Exception ex) {
+                log.error("Could not close the channel", ex);
+            }
+        }
+
+        // Close connection.
+        if (connection != null) {
+            try {
+                log.info(String.format("Going to close connection: %s", connection));
+
+                connection.close();
+                connection = null;
+            } catch (Exception ex) {
+                log.error("Could not close the connection", ex);
+            }
+        }
+    }
+
+    public void doTheJob() {
+        try {
+            this.ensureConnected();
+
+            final QueueingConsumer consumer = new QueueingConsumer(channel);
+            channel.basicConsume(QUEUE_XMPP_NAME, true, consumer);
+            while (this.running) {
+                final QueueingConsumer.Delivery delivery = consumer.nextDelivery(2000);
+                if (delivery == null) {
+                    continue;
+                }
+
+                processNewMessage(delivery);
+            }
+        } catch (Exception ex) {
+            log.warn("Problem occurred during property sync", ex);
+        } finally {
+            ensureClosed();
+        }
+    }
+
     @Override
     public void run() {
         this.running = true;
         while (this.running) {
-            long cmilli = System.currentTimeMillis();
-            if ((cmilli-lastSync) > TIMEOUT_LISTEN){
-                lastSync = cmilli;
-                doTheJob();
-            }
+            doTheJob();
 
             try {
-                Thread.sleep(5000);
+                Thread.sleep(2000);
             } catch (InterruptedException ex) {
                 log.error("Interrupted", ex);
                 break;
             }
         }
-        log.info("AMPQListener thread ended.");
+
+        ensureClosed();
+        log.info(String.format("AMPQListener thread ended. Running: %s, this: %s", running, this));
     }
 
-    private void processNewMessage(QueueingConsumer.Delivery delivery){
+    private void processNewMessage(QueueingConsumer.Delivery delivery) {
         try {
-            if (listener != null){
+            if (listener != null) {
                 listener.acceptMessage(QUEUE_XMPP_NAME, delivery);
             }
-        } catch(Exception ex){
+        } catch (Exception ex) {
             log.warn("Exception in processing a new message");
         }
     }
-    
+
     public boolean isRunning() {
         return running;
     }
@@ -161,7 +172,7 @@ public class AMQPListener extends BackgroundThreadService {
 
     public void setListener(AMQPMsgListener listener) {
         this.listener = listener;
-        log.info("Listener set");
+        log.info(String.format("Listener set to %s, listener %s", this, listener));
     }
-    
+
 }
